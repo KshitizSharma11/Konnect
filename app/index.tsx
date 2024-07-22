b321d51fc0c451ccabc1bcebb1692e878f69ec2e
KshitizSharma11 <kshitizsharma.00@gmail.com>
2024-07-22 11:37:04 +0530

    updated the landing UI to be more attractive

--- app/index.tsx
+++ app/index.tsx
@@ -17,10 +17,10 @@ export default function Index() {
       <View>
   <Text className="text-center text-4xl text-center font-bold">Let's explore new people around you</Text>
   </View>
-  <View className="space-y-6 m-2">
-  <CustomButton onPress={()=>console.log("pressed")} containerStyles="bg-black m-5 mb-3 flex-row rounded-full bg-[#4b164c]" textStyles="text-white font-bold" title="Login with Phone" imageLogo={callMob} imageStyles="transform -translate-x-12" />
-    <CustomButton onPress={()=>console.log("pressed")} containerStyles="bg-black m-5 mt-2 flex-row  rounded-full bg-purple-50" textStyles="text-purple-900 font-bold" title="Login with Google" imageLogo={googleIco} imageStyles="transform -translate-x-12" />
-      <Text className="mb-10 text-center">Don't have an account? <Text className="text-purple-400 font-medium">Sign Up</Text></Text>
+  <View className="m-2">
+  <CustomButton onPress={()=>console.log("pressed")} containerStyles="bg-black m-3 mb-3 flex-row rounded-full bg-[#4b164c]" textStyles="text-white font-bold" title="Login with Phone" imageLogo={callMob} imageStyles="transform -translate-x-12" />
+    <CustomButton onPress={()=>console.log("pressed")} containerStyles="bg-black m-3 mt-2 flex-row  rounded-full bg-purple-50" textStyles="text-purple-900 font-bold" title="Login with Google" imageLogo={googleIco} imageStyles="transform -translate-x-12" />
+      <Text className="mb-10 text-center mt-2">Don't have an account? <Text className="text-purple-400 font-medium">Sign Up</Text></Text>
   </View>
   <StatusBar style="light"/>
       </SafeAreaView>
--- components/LoginCircle.tsx
+++ components/LoginCircle.tsx
@@ -1,16 +1,33 @@
 import { View, Text,Image } from 'react-native'
 import React from 'react'
-import girlImage from "@/assets/images/girl.jpeg";
-
+import model3 from "@/assets/images/model3.png";
+import Girl from "@/assets/images/model1.webp";
+import modelOne from "@/assets/images/images.jpeg";
+import modelBoy from "@/assets/images/adiBro.png";
+import modelGuy from "@/assets/images/modeBoy.jpg";
+import modelMe from "@/assets/images/me.jpg";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { FontAwesome6 } from '@expo/vector-icons';
 const LoginCircle = () => {
   return (
-    <View className='items-center'>
-     <View className='border border-purple-100 rounded-full border-dotted  w-72 h-72 p-4 items-center justify-center'>
-        <View className=' bg-white rounded-full w-64 h-64 items-center justify-center'>
-        <View className='bg-purple-100 rounded-full w-60 h-60 items-center justify-center'>
+    <View className='items-center mb-10'>
+     <View className='border border-purple-100 rounded-full border-dotted  w-86 h-86 p-6 items-center justify-center'>
+        <View className=' bg-white rounded-full w-72 h-72 items-center justify-center'>
+        <View className='bg-[#faedf8] rounded-full w-64 h-64 items-center justify-center'>
+        <Image source={modelGuy} className='rounded-full w-10 h-10 transform -translate-y-28 -translate-x-4 absolute z-10 border-4 border-purple-300'/>
+        <Image source={modelOne} className='rounded-full w-10 h-10 transform translate-y-[100px] translate-x-14 absolute z-10 border-4 border-purple-300'/>  
+        <Image source={modelBoy} className='rounded-full w-14 h-14 transform translate-y-20 -translate-x-36 absolute z-10 border-4 border-purple-300'/>  
+        <Image source={modelMe} className='rounded-full w-14 h-14 transform translate-y-20 translate-x-36 absolute z-10 border-2 border-purple-300'/>
+        <Image source={model3} className='rounded-full w-14 h-14 transform translate-y-40  absolute z-10 border-2 border-purple-300'/>
+       <View className='transform translate-y-36 -translate-x-24 z-10 absolute'>
+        <Ionicons name="chatbox-ellipses"  size={28} color="#dd88cf" />
+        </View>
+        <View className='transform -translate-y-36 translate-x-20 z-10 absolute'>
+        <FontAwesome6 name="location-dot" size={28} color="#dd88cf" />
+        </View>
             <View className='bg-white rounded-full items-center justify-center w-40 h-40'>
-            <View className='bg-pink-200 rounded-full items-center justify-center w-28 h-28'>
-                <Image source={girlImage} className='rounded-full w-24 h-24 border-4 border-purple-300'/>
+            <View className='bg-pink-200 rounded-full items-center justify-center w-24 h-24'>
+                <Image source={Girl} className='rounded-full w-24 h-24 border-4 border-purple-300'/>
                 </View>
             </View>
         </View>
